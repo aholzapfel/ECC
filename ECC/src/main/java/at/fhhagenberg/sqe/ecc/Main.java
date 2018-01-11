311e75e308436e59cd8b61a23267ee5d35dd2ad1
Roman Benetseder <roman.benetseder@gmail.com>
2018-01-11 13:04:46 +0100

    Code refactoring

--- ECC/src/main/java/at/fhhagenberg/sqe/ecc/Main.java
+++ ECC/src/main/java/at/fhhagenberg/sqe/ecc/Main.java
@@ -1,6 +1,5 @@
 package at.fhhagenberg.sqe.ecc;
 	
-import java.io.IOException;
 import java.net.URL;
 import java.rmi.RemoteException;
 import java.util.concurrent.Executors;
@@ -8,12 +7,10 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import at.fhhagenberg.sqe.ecc.controller.ElevatorControlCenterController;
-import at.fhhagenberg.sqe.ecc.controller.ElevatorController;
 import at.fhhagenberg.sqe.ecc.sqelevator.ElevatorControlCenter;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.fxml.FXMLLoader;
 
@@ -23,15 +20,15 @@ public class Main extends Application {
 	private static final int WINDOW_HEIGHT = 500;
 	private static final int WINDOW_WIDTH = 800;
 	
-	private final int numberOfElevators = 4;
-	private final int numberOfFloors = 4;
+	private static final int NUMBER_OF_FLOORS = 4;
+	private static final int NUMBER_OF_ELEVATORS = 4;
 	
 	ElevatorControlCenterController controller;
 	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			ElevatorControlCenter ecc = new ElevatorControlCenter(numberOfFloors, numberOfElevators);
+			ElevatorControlCenter ecc = new ElevatorControlCenter(NUMBER_OF_FLOORS, NUMBER_OF_ELEVATORS);
 			
 			URL location = Main.class.getClassLoader().getResource("layouts\\elevator_control_center.fxml");
 			
