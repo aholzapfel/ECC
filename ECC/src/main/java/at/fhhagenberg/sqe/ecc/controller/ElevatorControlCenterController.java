78997ebf04de6706cf68ee7479d1c19712237875
JohannesUebleis <uebleis.johannes@gmx.at>
2018-01-18 11:57:33 +0100

    clocktime

--- ECC/src/main/java/at/fhhagenberg/sqe/ecc/controller/ElevatorControlCenterController.java
+++ ECC/src/main/java/at/fhhagenberg/sqe/ecc/controller/ElevatorControlCenterController.java
@@ -17,32 +17,36 @@ import javafx.util.Callback;
 import sqelevator.IElevator;
 
 public class ElevatorControlCenterController {
-	
-    @FXML private ListView<Floor> floors;
-    @FXML private FloorsController floorsController;
-    
-	@FXML private ListView<Elevator> lvElevators;
-	
+
+	@FXML
+	private ListView<Floor> floors;
+	@FXML
+	private FloorsController floorsController;
+
+	@FXML
+	private ListView<Elevator> lvElevators;
+
 	protected List<Elevator> elevators = new ArrayList<>();
 	protected ListProperty<Elevator> listPropertyElevators = new SimpleListProperty<>();
-	
+
 	private List<ElevatorsListViewCell> cells = new ArrayList<>();
-	
+
 	private IElevator elevatorSystem;
-	
+	private long clockTime;
+
 	public void init(IElevator elevatorSystem) {
 		try {
 			this.elevatorSystem = elevatorSystem;
-			
+
 			floorsController.init(elevatorSystem);
-			
-			for(int i = 0; i <= elevatorSystem.getElevatorNum()-1; i++) {
+
+			for (int i = 0; i <= elevatorSystem.getElevatorNum() - 1; i++) {
 				elevators.add(initElevatorFromSystem(new Elevator(i)));
 			}
-		
+
 			lvElevators.itemsProperty().bind(listPropertyElevators);
 			lvElevators.setCellFactory(new Callback<ListView<Elevator>, ListCell<Elevator>>() {
-				
+
 				@Override
 				public ListCell<Elevator> call(ListView<Elevator> param) {
 					ElevatorsListViewCell cell = new ElevatorsListViewCell(elevatorSystem);
@@ -50,47 +54,53 @@ public class ElevatorControlCenterController {
 					return cell;
 				}
 			});
-	        
+
 			listPropertyElevators.set(FXCollections.observableArrayList(elevators));
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public Runnable updateRunnable = new Runnable() {
 		public void run() {
+
 			try {
-				// Go throw all elevators and reload their data from elevator system
-				// Refreshes GUI automatically through binding
-				for(Elevator elevator : elevators) {
-					initElevatorFromSystem(elevator);
+
+				if (clockTime != elevatorSystem.getClockTick()) {
+					// Go throw all elevators and reload their data from elevator system
+					// Refreshes GUI automatically through binding
+					for (Elevator elevator : elevators) {
+						initElevatorFromSystem(elevator);
+					}
+
+					// Refresh floor list
+					floorsController.refreshFloors();
+
+					// Refresh elevator floor lists
+					for (ElevatorsListViewCell cell : cells) {
+						cell.refreshFloors();
+					}
 				}
-				
-				// Refresh floor list
-				floorsController.refreshFloors();
-				
-				// Refresh elevator floor lists
-				for(ElevatorsListViewCell cell : cells) {
-					cell.refreshFloors();
-				}
-				
+
+				clockTime = elevatorSystem.getClockTick();
+
 			} catch (RemoteException e) {
 				e.printStackTrace();
 			}
-	    }
+		}
 	};
-	
+
 	private Elevator initElevatorFromSystem(Elevator elevator) throws RemoteException {
 		int number = elevator.getNumber();
-		
+
 		int payload = elevatorSystem.getElevatorWeight(number);
 		int speed = elevatorSystem.getElevatorSpeed(number);
 		int doorStatus = elevatorSystem.getElevatorDoorStatus(number);
-		
+
 		elevator.setPayload(payload);
 		elevator.setSpeed(speed);
 		elevator.setDoorStatus(doorStatus);
-		
+
 		return elevator;
 	}
 }
